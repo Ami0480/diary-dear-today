bec6c7bac5132e1f5db9cd95ca693692f3faeb8b
Ami Fukuyama <amifuku80@gmail.com>
2026-01-23 14:00:48 +0800

    Installed supabase

---
+++ my-app/src/supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from "npm:@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
